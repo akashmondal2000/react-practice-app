6ea6d19de7bdaf275005ff4c184a691176f0f3c9
akashmondal2000 <mondalakash494@gmail.com>
2026-07-01 17:03:06 +0530

    add and structured loder using react router

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import RootLayout from "./pages/Root";
 import EventsRootsLayout from "./pages/EventsRoots";
 import HomePage from "./pages/Home";
-import EventsPage from "./pages/Events";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
@@ -16,13 +16,17 @@ const App = () => {
         { index: true, element: <HomePage /> }, // this is called index route
 
         // hare i will remove "/" for creating there "reletive" path
-        
+
         {
           path: "events",
           element: <EventsRootsLayout />,
           // hare i will create nested roots
           children: [
-            { index: true, element: <EventsPage /> },
+            {
+              index: true,
+              element: <EventsPage />,
+              loader: eventsLoader,
+            },
             { path: ":eventId", element: <EventDetailPage /> },
             { path: "new", element: <NewEventPage /> },
             { path: ":eventId/edit", element: <EditEventPage /> },
--- frontend-app/src/pages/Events.jsx
+++ frontend-app/src/pages/Events.jsx
@@ -1,46 +1,24 @@
-import { Link } from "react-router";
-import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router";
 import EventsList from "../components/EventsList.jsx";
 
-const DUMMY_EVENTS = [
-  { id: "e1", title: "Event Detail 1" },
-  { id: "e2", title: "Event Detail 2" },
-];
-
 const EventsPage = () => {
-  const [fetchedEvents, setFetchedEvents] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError ] = useState('');
-
-  useEffect(() => {
-    const fetchEvents = async () => {
-
-      setIsLoading(true);
-      const responce = await fetch("http://localhost:8080/events");
-
-      if (!responce.ok){
-        setError('Fetching events faild.')
-      }else{
-        const resData = await responce.json();
-        setFetchedEvents(resData.events);
-      }
-      setIsLoading(false);
-    };
-
-    fetchEvents();
-  }, []);
-
+  const events = useLoaderData();
   return (
     <>
-      <h1>Events Page</h1>
-
-      <div style={{ textAlign: "center" }}>
-        {isLoading && <p>Loading....</p>  }
-        { error && <p>{error}</p> }
-      </div>
-      <EventsList events={fetchedEvents} />
+      <EventsList events={events} />
     </>
   );
 };
 
 export default EventsPage;
+
+export const loader = async () => {
+  const responce = await fetch("http://localhost:8080/events");
+
+  if (!responce.ok) {
+    //.....
+  } else {
+    const resData = await responce.json();
+    return resData.events;
+  }
+};
