3aae768feb57908a56b102b445773c158db347cb
akashmondal2000 <mondalakash494@gmail.com>
2026-07-01 14:59:25 +0530

    fetched data, render events list, set loading and error state

--- frontend-app/src/pages/Events.jsx
+++ frontend-app/src/pages/Events.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router";
+import { useEffect, useState } from "react";
+import EventsList from "../components/EventsList.jsx";
 
 const DUMMY_EVENTS = [
   { id: "e1", title: "Event Detail 1" },
@@ -6,16 +8,37 @@ const DUMMY_EVENTS = [
 ];
 
 const EventsPage = () => {
+  const [fetchedEvents, setFetchedEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError ] = useState('');
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+
+      setIsLoading(true);
+      const responce = await fetch("http://localhost:8080/events");
+
+      if (!responce.ok){
+        setError('Fetching events faild.')
+      }else{
+        const resData = await responce.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
     <>
       <h1>Events Page</h1>
-      <ul>
-        {DUMMY_EVENTS.map((eventItem) => (
-          <li key={eventItem.id}>
-            <Link to={eventItem.id}>{eventItem.title}</Link>
-          </li>
-        ))}
-      </ul>
+
+      <div style={{ textAlign: "center" }}>
+        {isLoading && <p>Loading....</p>  }
+        { error && <p>{error}</p> }
+      </div>
+      <EventsList events={fetchedEvents} />
     </>
   );
 };
