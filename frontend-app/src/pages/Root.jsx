9ac1e21ae16f422c8f7969e660296935ac0df7d2
akashmondal2000 <mondalakash494@gmail.com>
2026-07-03 10:16:58 +0530

    slow responce from the backend for testing, check currently loading state

--- frontend-app/src/pages/Root.jsx
+++ frontend-app/src/pages/Root.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router";
+import { Outlet,useNavigation } from "react-router";
 import MainNavigation from "../components/MainNavigation";
 
 const RootLayout = () => {
+  const navigation = useNavigation();
   return (
     <>
       <MainNavigation />
       <main>
+        { navigation.state === 'loading' && <p>Loading...</p> }
         <Outlet />
       </main>
     </>
