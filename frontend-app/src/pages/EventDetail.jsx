68a85702123281b2df1994b70320cb12766dcf65
akashmondal2000 <mondalakash494@gmail.com>
2026-07-12 15:24:08 +0530

    track loading or pending state using useNavigation

--- frontend-app/src/pages/EventDetail.jsx
+++ frontend-app/src/pages/EventDetail.jsx
@@ -15,7 +15,7 @@ export default EventDetailPage;
 
 export const loader = async ({ request, params }) => {
   const id = params.eventId;
-  const response = await fetch("http://localhost:8080/events/" + id);
+  const response = await fetch("http://localhost:8080/events/"+id);
 
   if (!response.ok) {
     throw new Response(
